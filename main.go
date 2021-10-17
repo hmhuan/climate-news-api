1ee2ce497224b2c7743ea24d2f507601edd1ece1
hmhuan <huan.huynh@linecorp.com>
2021-10-18 00:00:46 +0900

    using parse string to html

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
+	"golang.org/x/net/html"
+)
+
+func main() {
+	var urls = []string{"https://theguardian.com/environment/climate-crisis"}
+
+	resp, err := http.Get(urls[0])
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	// fmt.Println(string(body))
+
+	htmlDoc, err := html.Parse(strings.NewReader(string(body)))
+
+	fmt.Println(htmlDoc.Data)
 }
